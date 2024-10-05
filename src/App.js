bd36c56f5e4dc871f768c3a975c79c9cb297b544
Zidan <muhdzidan007@gmail.com>
2024-10-06 02:55:33 +0530

    converted js to jsx

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MusicPlayer from "./Components/MusicPlayer"; // Ensure the import path is correct
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<MusicPlayer />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
+const App = () => (
+  <Router>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<MusicPlayer />} />
+      </Routes>
+    </div>
+  </Router>
+);
 
 export default App;
+
